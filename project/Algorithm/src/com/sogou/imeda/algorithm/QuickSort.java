4aa2f49f81f8c237d05824c61d1a93a5303886b1
Betty <betty_zbl@163.com>
2017-11-20 23:51:41 +0800

    modify by bbl

--- project/Algorithm/src/com/sogou/imeda/algorithm/QuickSort.java
+++ project/Algorithm/src/com/sogou/imeda/algorithm/QuickSort.java
@@ -1,5 +1,38 @@
 package com.sogou.imeda.algorithm;
 
 public class QuickSort {
+	public static void main(String[] args) {
+		int[] array = { 5, 2, 6, 8, 1, 5, 8, 3, 0, 4, 7, 2 };
+		printArray(array);
+		sortRecursion(array, 0, array.length - 1);
+	}
 
+	private static void sortRecursion(int[] array, int start, int end) {
+		int mid = array[start];
+		int midIndex = start;
+		for (int i = 1; i <= end - start; i++) {
+			int tmp = array[i];
+			if (tmp < mid) {
+				swap(array, i, midIndex);
+			}
+		}
+
+	}
+
+	private static void swap(int[] array, int i, int midIndex) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void printArray(int[] array) {
+		String output = "NULL";
+		for (int a : array) {
+			if (output.equals("NULL")) {
+				output = String.valueOf(a);
+			} else {
+				output += "," + String.valueOf(a);
+			}
+		}
+		System.out.println(output);
+	}
 }
