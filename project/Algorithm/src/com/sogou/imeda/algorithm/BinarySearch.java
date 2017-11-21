49c2a76dc274aa1ded06937ec40a13e75dba80de
Betty <betty_zbl@163.com>
2017-11-21 22:54:43 +0800

    bbl

--- project/Algorithm/src/com/sogou/imeda/algorithm/BinarySearch.java
+++ project/Algorithm/src/com/sogou/imeda/algorithm/BinarySearch.java
@@ -3,25 +3,26 @@ package com.sogou.imeda.algorithm;
 public class BinarySearch {
 
 	public static void main(String[] args) {
-		int[] a = { 2, 5, 7, 9, 10, 25, 47, 69 };
-		int b = 7;
-		int index = biSearch(a, 0, a.length - 1, b);
+		int[] array = { 2, 5, 7, 9, 10, 25, 47, 69 };
+		int index = biSearch(array, 0, array.length - 1, 10);
+		System.out.println(index);
 	}
 
 	private static int biSearch(int[] a, int start, int end, int b) {
-
-		if (start == end) {
-			return -1;
-		}
-		int mid = a.length / 2;
-		if (b == a[mid]) {
+		int mid = (end - start) / 2 + start;
+		System.out.println("start=" + start + ";end=" + end + ";mid=" + mid
+				+ ";b=" + b + ";a[mid]=" + a[mid]);
+		if (a[mid] == b) {
 			return mid;
 		}
+		if (start >= end) {
+			return -1;
+		}
 		if (b < a[mid]) {
-			biSearch(a, 0, mid - 1, b);
-		} else {
+			biSearch(a, start, mid - 1, b);
+		} else if (b > a[mid]) {
 			biSearch(a, mid + 1, end, b);
 		}
-		return 0;
+		return -2;
 	}
 }
